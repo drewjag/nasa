9720645450ea15cd7660b6941c3dce10ee990392
Chris Kelly <ChrisK@achievers.com>
2014-04-13 17:45:01 -0400

    Compare libraries use base measurement ratios to compare measurements equally then compares to determine if asteroid is larger in comparison type

--- application/libraries/compare_library.php
+++ application/libraries/compare_library.php
@@ -21,7 +21,7 @@ class Compare_library
 
         if ($compare_object['name'] == 'Circumference')
         {
-            $compare_object['num_objects'] = $this->calculate_conversion_circumference($compare_object,$asteroid);
+            list($compare_object['num_objects'],$compare_object['object_larger']) = $this->calculate_conversion_circumference($compare_object,$asteroid);
         }
         elseif ($compare_object['name'] == 'Diameter')
         {
@@ -29,7 +29,7 @@ class Compare_library
         }
         elseif ($compare_object['name'] == 'Composition')
         {
-            $compare_object['num_objects'] = $this->calculate_conversion_composition($compare_object,$asteroid);
+            list($compare_object['num_objects'],$compare_object['object_larger']) = $this->calculate_conversion_composition($compare_object,$asteroid);
         }
 
         return $compare_object;
@@ -37,7 +37,17 @@ class Compare_library
 
     function calculate_conversion_circumference($object,$asteroid)
     {
+        //Circumference Calculation
+        $base_object_value = $object['base_measurement_ratio'] * $object['measurement_value'];
 
+        //Diameter is in km in database
+        $base_asteroid_value = $asteroid['diameter'] * 1000;
+        $radius = $base_asteroid_value / 2;
+        $pie = 3.14159265359;
+
+        $circumference_of_asteroid_base = 2 * $pie * $radius;
+
+        return $this->is_asteroid_larger($base_object_value, $circumference_of_asteroid_base);
     }
 
     function calculate_conversion_diameter($object,$asteroid)
@@ -48,6 +58,31 @@ class Compare_library
         //Diameter is in km in database
         $base_asteroid_value = $asteroid['diameter'] * 1000;
 
+        return $this->is_asteroid_larger($base_object_value, $base_asteroid_value);
+    }
+
+    function calculate_conversion_composition($object,$asteroid)
+    {
+        $base_object_value = $object['base_measurement_ratio'] * $object['measurement_value'];
+
+        //Mass is in cm3
+        $base_asteroid_value = $asteroid['mass'];
+
+        return $this->is_asteroid_larger($base_object_value, $base_asteroid_value);
+    }
+
+    function calculate_conversion_volume($object,$asteroid)
+    {
+        $base_object_value = $object['base_measurement_ratio'] * $object['measurement_value'];
+
+        //Mass is in litres
+        $base_asteroid_value = $asteroid['volume'];
+
+        return $this->is_asteroid_larger($base_object_value, $base_asteroid_value);
+    }
+
+    function is_asteroid_larger($base_object_value, $base_asteroid_value)
+    {
         if ($base_object_value > $base_asteroid_value)
         {
             $object_larger = true;
@@ -59,11 +94,6 @@ class Compare_library
         $num_of_objects = $base_asteroid_value / $base_object_value;
 
         return array($num_of_objects, $object_larger);
-    }
-
-    function calculate_conversion_composition($object,$asteroid)
-    {
-
     }
 
 }
